c1b12d162d3e40421d9c2028cb53ae9d0262c3a2
Tressie Muthuri <tressiemuthuri@gmail.com>
2022-11-24 21:05:47 +0300

    Completed assignment

--- app/controllers/dog_houses_controller.rb
+++ app/controllers/dog_houses_controller.rb
@@ -3,7 +3,9 @@ class DogHousesController < ApplicationController
 
   def show
     dog_house = DogHouse.find(params[:id])
-    render json: dog_house
+    # render json: dog_house
+    render json: dog_house, include: :reviews
+
   end
 
   private
